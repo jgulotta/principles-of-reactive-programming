e4dbe3f95b95603dc105ca230bda000d03983d16
Jacob Gulotta <jgulotta@gmail.com>
2013-11-18 09:57:34 -0800

    Initial commit with or gates completed

---
+++ simulations/src/main/scala/simulations/Circuit.scala
@@ -0,0 +1,103 @@
+package simulations
+
+class Wire {
+  private var sigVal = false
+  private var actions: List[Simulator#Action] = List()
+
+  def signal: Boolean = sigVal
+  
+  def signal_=(s: Boolean) {
+    if (s != sigVal) {
+      sigVal = s
+      actions.foreach(action => action())
+    }
+  }
+
+  def addAction(a: Simulator#Action) {
+    actions = a :: actions
+    a()
+  }
+}
+
+abstract class CircuitSimulator extends Simulator {
+  val InverterDelay: Int
+  val AndGateDelay: Int
+  val OrGateDelay: Int
+
+  def probe(name: String, wire: Wire) {
+    wire addAction {
+      () => afterDelay(0) {
+        println(
+          "  " + currentTime + ": " + name + " -> " +  wire.signal)
+      }
+    }
+  }
+
+  def inverter(input: Wire, output: Wire) {
+    def invertAction() {
+      val inputSig = input.signal
+      afterDelay(InverterDelay) { output.signal = !inputSig }
+    }
+    input addAction invertAction
+  }
+
+  def andGate(a1: Wire, a2: Wire, output: Wire) {
+    def andAction() {
+      val a1Sig = a1.signal
+      val a2Sig = a2.signal
+      afterDelay(AndGateDelay) { output.signal = a1Sig & a2Sig }
+    }
+    a1 addAction andAction
+    a2 addAction andAction
+  }
+
+  def orGate(a1: Wire, a2: Wire, output: Wire) {
+    def orAction() {
+      val a1Sig = a1.signal
+      val a2Sig = a2.signal
+      afterDelay(OrGateDelay) { output.signal = a1Sig | a2Sig }
+    }
+    a1 addAction orAction
+    a2 addAction orAction
+  }
+  
+  def orGate2(a1: Wire, a2: Wire, output: Wire) {
+    val a1n, a2n, a1n_a2n = new Wire
+    inverter(a1, a1n)
+    inverter(a2, a2n)
+    andGate(a1n, a2n, a1n_a2n)
+    inverter(a1n_a2n, output)
+  }
+
+  def demux(in: Wire, c: List[Wire], out: List[Wire]) {
+    ???
+  }
+}
+
+object Circuit extends CircuitSimulator {
+  val InverterDelay = 1
+  val AndGateDelay = 3
+  val OrGateDelay = 5
+
+  def andGateExample() {
+    val in1, in2, out = new Wire
+    andGate(in1, in2, out)
+    probe("in1", in1)
+    probe("in2", in2)
+    probe("out", out)
+    in1.signal = false
+    in2.signal = false
+    run
+
+    in1.signal = true
+    run
+
+    in2.signal = true
+    run
+  }
+}
+
+object CircuitMain extends App {
+  // You can write tests either here, or better in the test class CircuitSuite.
+  Circuit.andGateExample()
+}
---
+++ simulations/src/test/scala/simulations/CircuitSuite.scala
@@ -0,0 +1,83 @@
+package simulations
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CircuitSuite extends CircuitSimulator with FunSuite {
+  val InverterDelay = 1
+  val AndGateDelay = 3
+  val OrGateDelay = 5
+
+  test("andGate example") {
+    val in1, in2, out = new Wire
+    andGate(in1, in2, out)
+    in1.signal = false
+    in2.signal = false
+    run
+
+    assert(out.signal === false, "and 1")
+
+    in1.signal = true
+    run
+
+    assert(out.signal === false, "and 2")
+
+    in2.signal = true
+    run
+
+    assert(out.signal === true, "and 3")
+  }
+
+  test("orGate example") {
+    val in1, in2, out = new Wire
+    orGate(in1, in2, out)
+    in1.signal = false
+    in2.signal = false
+    run
+
+    assert(out.signal === false, "or 1")
+
+    in1.signal = true
+    run
+
+    assert(out.signal === true, "or 2")
+
+    in2.signal = true
+    run
+
+    assert(out.signal === true, "or 3")
+
+    in1.signal = false
+    run
+
+    assert(out.signal === true, "or 4")
+  }
+
+  test("orGate2 example") {
+    val in1, in2, out = new Wire
+    orGate2(in1, in2, out)
+    in1.signal = false
+    in2.signal = false
+    run
+
+    assert(out.signal === false, "or2 1")
+
+    in1.signal = true
+    run
+
+    assert(out.signal === true, "or2 2")
+
+    in2.signal = true
+    run
+
+    assert(out.signal === true, "or2 3")
+
+    in1.signal = false
+    run
+
+    assert(out.signal === true, "or2 4")
+  }
+}
